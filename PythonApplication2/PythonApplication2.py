d8ea4ed674d3e8362983bdb7cb10da7d7491d687
BungPeerapat <mashiro74122541@gmail.com>
2020-09-19 10:57:14 +0700

    Change Way

---
+++ PythonApplication2/PythonApplication2.py
@@ -0,0 +1,37 @@
+#x = int(input("Please Input Number : "))
+#name = str(input("Your name is : "))
+#Age = int(input("Please input Age : "))
+#y = int(12)
+#z = int(1)
+
+##while z <= y:
+##    print(x , " * " , z , " = " , x*z)
+##    z = z + 1
+    
+##    if z == y:
+##        print("My Name is {}".format(name))
+##        myage = "My Age is {} ".format(Age)
+
+##        print("Your Age is : " + myage[11])
+
+def calculator():
+
+    x = int(input("please input number : "))
+    name = str(input("your name is : "))
+    age = int(input("please input age : "))
+    y = int(12)
+    z = int(1)
+
+    while z <= y:
+        print(x , " * " , z , " = " , x*z)
+        z = z + 1
+    
+        if z == y:
+            print("my name is {}".format(name))
+            myage = "my age is {} ".format(age)
+
+            print("your age is : " + myage[11])
+            print("===========================")
+            calculator()
+
+calculator()
